ebb612110dcb24bf30a460b3189a2df4dc85b643
Matheusy1997 <matheusmachado.dev@gmail.com>
2024-11-28 11:52:31 -0300

    Initial commit

---
+++ aula01- operador ternario/aula01.js
@@ -0,0 +1,9 @@
+let num1=10;
+let num2=5;
+res=(num1 > num2 ? "Verdadeiro" : "Falso")
+console.log(res)
+
+//0 = False
+//1 = True
+
+// Teste lógico ? se verdadeiro : se Falso
---
+++ aula04 - Switch Case/aula04.js
@@ -0,0 +1,17 @@
+let colocacao=10;
+
+switch(colocacao){
+    case 1:
+        console.log("Primeiro lugar")
+        break;
+    case 2:
+        console.log("Segundo lugar")
+        break;
+    case 3:
+        console.log("Terceiro lugar")
+        break;
+    case 4: case 5: case 6:
+        console.log("Premio de participação")
+    default:
+        console.log("Não subiu ao podio")
+}
