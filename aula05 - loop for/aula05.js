fc3f36d94a816db52229db38b7d9d9a95d13415f
Matheusy1997 <matheusmachado.dev@gmail.com>
2024-11-28 16:00:07 -0300

    aula05 criada

---
+++ aula05 - loop for/aula05.js
@@ -0,0 +1,9 @@
+console.log("Inicio do Programa")
+for(let i=0; i <= 50; i++){
+    if(i%2 ==0){
+        console.log(i + " e Par")
+    }else{
+        console.log(i + "e Impar")
+    }
+}
+console.log("Fim do programa")
