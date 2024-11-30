b8706f1d819b5e4f6cf50fd022620895aab40e38
Matheusy1997 <matheusmachado.dev@gmail.com>
2024-11-30 18:12:34 -0300

    aula 07 e 08 criadas

---
+++ aula07- funções/aula07.js
@@ -0,0 +1,20 @@
+//forma tradicional de declara uma função
+
+// function nome(){
+//     console.log("CFB Cursos")
+// }
+//chamada tradicional da função
+// nome();
+
+//pegando um elemento do HTML
+
+function mudarTexto(){
+    let d1=document.getElementById('d1')
+    let d2=document.getElementById('d2')
+    let d3=document.getElementById('d3')
+
+    d1.innerHTML='Matheus de Oliveira'
+    d2.innerHTML='Matheus de Oliveira'
+    d3.innerHTML='Matheus de Oliveira'
+}
+
