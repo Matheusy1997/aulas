c3417f0a07f2bdc91253ccb9a6867f07683646cf
Matheusy1997 <matheusmachado.dev@gmail.com>
2025-01-07 18:44:32 -0300

    update

--- exercicios/capitulo 4 - condicionais/ex04-13 prompt/ex04-13.js
+++ exercicios/capitulo 4 - condicionais/ex04-13 prompt/ex04-13.js
@@ -4,18 +4,22 @@ const num1 = Number(prompt("Digite o Lado A: "));
 const num2 = Number(prompt("Digite o Lado B: "));
 const num3 = Number(prompt("Digite o Lado C: "));
 
-if (num1 + num2 > num3 && num1 + num3 > num2 && num2 + num3 > num1) {
-  if (num1 == num2 && num1 == num3 && num2 == num3) {
-    console.log("Triângulo Equilatero");
-  } else if (
-    (num1 == num2 && num2 != num3) ||
-    (num1 == num3 && num2 != num1) ||
-    (num2 == num3 && num2 != num1)
-  ) {
-    console.log("Triângulo Isoceles");
-  } else if (num1 != num2 && num1 != num3 && num2 != num3) {
-    console.log("Triângulo Escaleno");
+if (!isNaN(num1) && !isNaN(num2) && !isNaN(num3)) {
+  if (num1 + num2 > num3 && num1 + num3 > num2 && num2 + num3 > num1) {
+    if (num1 == num2 && num1 == num3 && num2 == num3) {
+      console.log("Triângulo Equilatero");
+    } else if (
+      (num1 == num2 && num2 != num3) ||
+      (num1 == num3 && num2 != num1) ||
+      (num2 == num3 && num2 != num1)
+    ) {
+      console.log("Triângulo Isoceles");
+    } else if (num1 != num2 && num1 != num3 && num2 != num3) {
+      console.log("Triângulo Escaleno");
+    }
+  } else {
+    console.log("Os lados não formam um triângulo");
   }
-} else {
-  console.log("Os lados não formam um triângulo");
+}else{
+    console.log("Erro... deve ser um número");
 }
