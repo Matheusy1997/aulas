4492dda318c6f63ba7585cc95a4caf6a86ebdd80
Matheusy1997 <matheusmachado.dev@gmail.com>
2025-01-14 17:34:19 -0300

    atualização

--- exercicios/capitulo 6 - vetores/ex06-5/ex06-5-4.js
+++ exercicios/capitulo 6 - vetores/ex06-5/ex06-5-4.js
@@ -3,6 +3,10 @@ const numbers = [5,4,3,2,1];
 
 const multiplica = numbers.reduce((acumulador, num) => acumulador * num, 1);     
 const multiplica2 = numbers.map(num => num * 2)
+const numberCres = numbers.map(num => num).sort((a,b) => a - b)
 
+
+console.log(numbers);
 console.log(multiplica);
 console.log(multiplica2);
+console.log(numberCres);
