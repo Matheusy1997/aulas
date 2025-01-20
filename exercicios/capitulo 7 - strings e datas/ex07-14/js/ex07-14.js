03a227b2430341a2ab83acddb05dced7aa572d1a
Matheusy1997 <matheusmachado.dev@gmail.com>
2025-01-20 16:03:55 -0300

    Ccapitulo 7

--- exercicios/capitulo 7 - strings e datas/ex07-14/js/ex07-14.js
+++ exercicios/capitulo 7 - strings e datas/ex07-14/js/ex07-14.js
@@ -6,25 +6,16 @@ frm.addEventListener("submit", (e) => {
     e.preventDefault(); // evita envio do form
     let dataInfracao = frm.inDataInfracao.value;
     let multa = Number(frm.inValorMulta.value);
-    let hoje = new Date();
-    let limite = new Date();
-
-    let partes = dataInfracao.split("-")
-    limite.setDate(Number(partes[2]))
-    limite.setMonth(Number(partes[1]) - 1)
-    limite.setFullYear(Number(partes[0]))
-
-    let diasAtraso = hoje - limite
-    let diasAtrasoEmDias = diasAtraso / 86400000
-
     
+    const dataIndracaoObj = new Date(dataInfracao);
+
+    dataIndracaoObj.setDate(dataIndracaoObj.getDate() + 90)
     
+    const dataLimite = dataIndracaoObj.toLocaleDateString("pt-BR")
+    const valorDesconto = multa - (multa * 0.20)   
 
-    let dataLimite = data.toLocaleDateString("pt-BR")
-    let valorDesconto = multa - (multa * 0.20)   
 
-
-    resp1.innerText = `Data Limite: ${dia}/${mes}/${ano}`
+    resp1.innerText = `Data Limite: ${dataLimite}`
     resp2.innerText = `Valor com Desconto: R$ ${valorDesconto.toFixed(2)}`
     
 })
